5317a6d682c1181af7f7f57e53d38abe95bc5c93
Ashiri <ajaoiyanu@gmail.com>
2018-02-23 03:41:49 +0100

    Initial commit

---
+++ wikipedia_reference_scraper.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_url_page(url):
+    page = requests.get(url)
+    if page.status_code == 200:
+        return page.text
+    else:
+        print('Check the url entered')
+
+
+def get_references(url, document_name):
+    page_text = get_url_page(url)
+
+    soup = BeautifulSoup(page_text, 'html.parser')
+    citations = soup.find_all('cite')
+
+    file = open(document_name, 'w')
+
+    for citation in citations:
+        file.write('\n\n')
+        for string in citation.strings:
+            file.write(string)
+
