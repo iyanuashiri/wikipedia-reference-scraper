03b178583df6f6a24294ac0980f02ce2576050a2
Ashiri <ajaoiyanu@gmail.com>
2018-02-26 12:57:04 +0100

    Add documentaion

--- wikipedia_reference_scraper.py
+++ wikipedia_reference_scraper.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 
 
 def get_url_page(url):
+    '''
+    This function creates a response object for the url that is
+    to be scrapped.
+
+    :param url: This is the url of the page that is to be scrapped
+    :return: IT returns the a response object of the url
+    '''
     page = requests.get(url)
     if page.status_code == 200:
         return page.text
@@ -11,6 +18,13 @@ def get_url_page(url):
 
 
 def get_references(url):
+    '''
+    This function calls the get_url_page() function.
+    The response from the get_url_page() function is parsed as html.
+
+    :param url: This is the url of the page that is to be scrapped
+    :return: The return object is an html object
+    '''
     page_text = get_url_page(url)
     soup = BeautifulSoup(page_text, 'html.parser')
     citations = soup.find_all('cite')
@@ -18,6 +32,14 @@ def get_references(url):
 
 
 def write_to_document(url, document_name):
+    '''
+    This is the main fucntion that calls the above fucntions and then does the actual write to
+    document.
+
+    :param url: This is the url of the page that is to be scrapped
+    :param document_name: The name of the document that our scrapped data will be written to
+    :return: There is no return object
+    '''
     with open(document_name, 'w') as file:
         for citation in get_references(url):
             file.write('\n\n')
