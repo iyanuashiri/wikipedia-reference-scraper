495ee3530357f5a570bf79ec12c9442c92267c9d
Ashiri <ajaoiyanu@gmail.com>
2018-02-26 03:22:19 +0100

    Refactored

--- wikipedia_reference_scraper.py
+++ wikipedia_reference_scraper.py
@@ -10,18 +10,26 @@ def get_url_page(url):
         print('Check the url entered')
 
 
-def get_references(url, document_name):
+def get_references(url):
     page_text = get_url_page(url)
-
     soup = BeautifulSoup(page_text, 'html.parser')
     citations = soup.find_all('cite')
+    return citations
 
+
+def write_to_document(url, document_name):
     with open(document_name, 'w') as file:
-
-        for citation in citations:
+        for citation in get_url_page(url):
             file.write('\n\n')
             for string in citation.strings:
                 file.write(string)
+
+
+
+
+
+
+
 
 
 
