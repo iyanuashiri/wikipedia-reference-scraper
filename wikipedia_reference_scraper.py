fb96c1b8789b2d48aeb9c0979bd074322dadc255
spkuehl <skuehlhorn@gmail.com>
2018-04-01 18:51:30 -0500

    Reworked wikipedia_reference_scraper.py to take command line arguments as an additional way to run the script. Updated README.md to reflect this new option. Command Line Arguments are accepted with the python-fire library and siz dependency, which is now reflected in requirements.txt

--- wikipedia_reference_scraper.py
+++ wikipedia_reference_scraper.py
@@ -1,59 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
+import fire
 
 
-def get_url_page(url):
-    '''
-    This function creates a response object for the url that is
-    to be scrapped.
+class WikipediaReferenceScraper(object):
 
-    :param url: This is the url of the page that is to be scrapped
-    :return: IT returns the a response object of the url
-    '''
-    page = requests.get(url)
-    if page.status_code == 200:
-        return page.text
-    else:
-        print('Check the url entered')
+    def get_url_page(self, url):
+        '''
+        This function creates a response object for the url that is
+        to be scrapped.
 
-
-def get_references(url):
-    '''
-    This function calls the get_url_page() function.
-    The response from the get_url_page() function is parsed as html.
-
-    :param url: This is the url of the page that is to be scrapped
-    :return: The return object is an html object
-    '''
-    page_text = get_url_page(url)
-    soup = BeautifulSoup(page_text, 'html.parser')
-    citations = soup.find_all('cite')
-    return citations
+        :param url: This is the url of the page that is to be scrapped
+        :return: IT returns the a response object of the url
+        '''
+        page = requests.get(url)
+        if page.status_code == 200:
+            return page.text
+        else:
+            print('Check the url entered')
 
 
-def write_to_document(url, document_name):
-    '''
-    This is the main fucntion that calls the above fucntions and then does the actual write to
-    document.
+    def get_references(self, url):
+        '''
+        This function calls the get_url_page() function.
+        The response from the get_url_page() function is parsed as html.
 
-    :param url: This is the url of the page that is to be scrapped
-    :param document_name: The name of the document that our scrapped data will be written to
-    :return: There is no return object
-    '''
-    with open(document_name, 'w') as file:
-        for citation in get_references(url):
-            file.write('\n\n')
-            for string in citation.strings:
-                file.write(string)
+        :param url: This is the url of the page that is to be scrapped
+        :return: The return object is an html object
+        '''
+        page_text = self.get_url_page(url)
+        soup = BeautifulSoup(page_text, 'html.parser')
+        citations = soup.find_all('cite')
+        return citations
 
 
+    def write_to_document(self, url, document_name):
+        '''
+        This is the main fucntion that calls the above fucntions and then does the actual write to
+        document.
+
+        :param url: This is the url of the page that is to be scrapped
+        :param document_name: The name of the document that our scrapped data will be written to
+        :return: There is no return object
+        '''
+        with open(document_name, 'w') as file:
+            for citation in self.get_references(url):
+                file.write('\n\n')
+                for string in citation.strings:
+                    file.write(string)
 
 
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+  fire.Fire(WikipediaReferenceScraper)
